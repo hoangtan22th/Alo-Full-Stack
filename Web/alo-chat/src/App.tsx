7c498f0eb577c453a7c7430c43cdc5063cb34043
hoangtan22th <hoangtan22th@gmail.com>
2026-04-04 22:57:48 +0700

    chore: setup cau truc thu muc React TS va Tailwind

---
+++ Web/alo-chat/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600 p-6 bg-white rounded-2xl shadow-lg">
+        Chào Tấn! Tailwind CSS đã hoạt động! 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
