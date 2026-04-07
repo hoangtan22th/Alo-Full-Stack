b53bfbf168eb33db5db36a9f124f90bc1d3e4c3c
TanDuy274 <phanduy27042004@gmail.com>
2026-04-07 23:40:51 +0700

    fix(auth service): qr code in redis

--- Backend/auth-service/src/main/java/edu/iuh/fit/auth_service/repository/QrSessionRepository.java
+++ Backend/auth-service/src/main/java/edu/iuh/fit/auth_service/repository/QrSessionRepository.java
@@ -1,12 +1,12 @@
 package edu.iuh.fit.auth_service.repository;
 
 import edu.iuh.fit.auth_service.entity.QrSession;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface QrSessionRepository extends JpaRepository<QrSession, String> {
+public interface QrSessionRepository extends CrudRepository<QrSession, String> {
     Optional<QrSession> findByQrToken(String qrToken);
 }
