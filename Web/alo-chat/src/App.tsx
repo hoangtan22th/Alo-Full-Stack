508ded6c0903f0bec546920b575b0bfd04efa7e4
hoangtan22th <hoangtan22th@gmail.com>
2026-04-07 18:24:51 +0700

    Refactor: update axios calls using axiosClient and implement route protection

--- Web/alo-chat/src/App.tsx
+++ Web/alo-chat/src/App.tsx
@@ -1,34 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import ContactLayout from '@/pages/contacts/ContactLayout';
-import LoginPage from '@/pages/auth/LoginPage';
-import RegisterPage from './pages/auth/RegisterPage';
-import FriendRequestPage from '@/pages/contacts/FriendRequestPage';
-import FriendListPage from '@/pages/contacts/FriendListPage';
-import { Toaster } from 'sonner';
+// src/App.tsx
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
+import AppRoutes from "@/routes/AppRoutes";
 
 function App() {
   return (
     <BrowserRouter>
-    {/* 2. Đặt Toaster ở đây để nó luôn lắng nghe mọi nơi */}
+      {/* Toaster đặt ở mức cao nhất để luôn hiển thị thông báo */}
       <Toaster position="top-right" richColors closeButton />
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
 
-        {/* Tuyến đường Danh bạ: Bọc Layout ở ngoài, các trang con bên trong */}
-        <Route path="/contacts" element={<ContactLayout />}>
-          {/* Mặc định gõ /contacts sẽ tự động nhảy vào /contacts/friends */}
-          <Route index element={<Navigate to="friends" replace />} />
-          
-          {/* URL: /contacts/friends */}
-          <Route path="friends" element={<FriendListPage />} />
-          
-          {/* URL: /contacts/requests */}
-          <Route path="requests" element={<FriendRequestPage />} />
-        </Route>
-
-      </Routes>
+      {/* Nạp toàn bộ Route vào đây */}
+      <AppRoutes />
     </BrowserRouter>
   );
 }
