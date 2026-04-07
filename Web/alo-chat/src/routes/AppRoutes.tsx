9cb57197bffe16d8fdc742529998eb0fd417a8db
hoangtan22th <hoangtan22th@gmail.com>
2026-04-08 01:56:21 +0700

    feat:create UI chat, groups

--- Web/alo-chat/src/routes/AppRoutes.tsx
+++ Web/alo-chat/src/routes/AppRoutes.tsx
@@ -7,6 +7,11 @@ import FriendListPage from "@/pages/contacts/FriendListPage";
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute"; // Import hàng rào mới
 import GroupListPage from "@/pages/contacts/GroupListPage";
+import ChatPage from "@/pages/chat/ChatPage";
+
+// IMPORT THÊM SIDEBAR ĐỂ GẮN VÀO TRANG CHAT
+import Sidebar from "@/components/ui/Sidebar"; // Chú ý kiểm tra lại đường dẫn này cho đúng với máy ông nhé
+import GroupInvitePage from "@/pages/contacts/GroupInvitePage";
 
 export default function AppRoutes() {
   return (
@@ -21,12 +26,34 @@ export default function AppRoutes() {
 
       {/* LỚP BẢO VỆ 2: Chỉ dành cho hội viên (Đã có token) */}
       <Route element={<ProtectedRoute />}>
+        {/* KHU VỰC DANH BẠ */}
         <Route path="/contacts" element={<ContactLayout />}>
           <Route index element={<Navigate to="friends" replace />} />
           <Route path="friends" element={<FriendListPage />} />
           <Route path="requests" element={<FriendRequestPage />} />
           <Route path="groups" element={<GroupListPage />} />
+          <Route path="group-invites" element={<GroupInvitePage />} />
         </Route>
+
+        {/* KHU VỰC CHAT (Đã được đưa ra ngoài, ngang hàng với contacts) */}
+        <Route
+          path="/chat"
+          element={
+            <div className="flex h-screen w-full overflow-hidden bg-white">
+              <Sidebar /> {/* Gắn thanh menu vào bên trái */}
+              <ChatPage />
+            </div>
+          }
+        />
+        <Route
+          path="/chat/:conversationId"
+          element={
+            <div className="flex h-screen w-full overflow-hidden bg-white">
+              <Sidebar />
+              <ChatPage />
+            </div>
+          }
+        />
       </Route>
 
       {/* Catch-all: Nếu gõ bậy bạ thì đá về login hoặc trang chủ tùy trạng thái */}
