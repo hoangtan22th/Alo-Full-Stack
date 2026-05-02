bc94266eb1b55c5c578028d774c7991257913ecb
tVhowww <nguyentrunghau7746@gmail.com>
2026-05-02 18:05:22 +0700

    chore: strict route guarding for admin

--- Backend/auth-service/src/main/java/edu/iuh/fit/auth_service/config/SecurityConfig.java
+++ Backend/auth-service/src/main/java/edu/iuh/fit/auth_service/config/SecurityConfig.java
@@ -8,10 +8,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
